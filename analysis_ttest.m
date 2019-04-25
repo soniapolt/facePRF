4783a217074860e7079bab74c682f8353a107ab5
Sonia Poltoratski <soniapolt@gmail.com>
2019-04-25 11:39:33 -0700

    add median median + mult tests option

--- analysis_ttest.m
+++ analysis_ttest.m
@@ -3,14 +3,16 @@
 clear all; close all;
 
 subjs = {'TH' 'DF' 'EM' 'JG' 'MG' 'SP'};
-expt = 'compPRF';
-test = 'size'; % can be parname (Y,X,sd,gain,exp,shift) or pRF.read value (r2,size,eccen,gain)
+expt = 'fixPRF';
+tests = {'Y' 'size' 'r2'}; % can be parname (Y,X,sd,gain,exp,shift) or pRF.read value (r2,size,eccen,gain)
+whichM = 'median'; % mean or median
+
 
 minR2 = 20;          % cutoff for vox selection
-ROIs= standardROIs('face');
-fitSuffix = '_new';
+ROIs= {'V1' 'hV4' 'IOG_faces' 'pFus_faces' 'mFus_faces'};
+fitSuffix = '';
 
-whichStim = 'photo';
+whichStim = 'internal';
 whichModel = 'kayCSS';
 hems = {'lh' 'rh'};
 
@@ -24,18 +26,21 @@ subjNum = cellNum(subjs,info.subjs);
 
 comps = nchoosek(1:length(roi(1).fits),2);
 
-for cc = 1:size(comps,1)
-    baseCond = comps(cc,1);
-    c = comps(cc,2);
-    comp(c).descr = [roi(1).fits(baseCond).cond ' vs ' roi(1).fits(c).cond];
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %  ttest the parameters
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    parNames = roi(1).fits(1).parNames;
-    testNum = cellNum(test,parNames);
-    
-    
+for t = 1:length(tests)
+    test = tests{t};
+    fprintf('\n\n');
+    for cc = 1:size(comps,1)
+        baseCond = comps(cc,1);
+        c = comps(cc,2);
+        comp(c).descr = [roi(1).fits(baseCond).cond ' vs ' roi(1).fits(c).cond];
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %  ttest the parameters
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        parNames = roi(1).fits(1).parNames;
+        testNum = cellNum(test,parNames);
+        
+        
         
         for r = 1:length(ROIs)
             sData = [];
@@ -46,13 +51,13 @@ for cc = 1:size(comps,1)
                 vox2 = subj(subjNum(s)).roi(ROInum(r)).fits(c).vox;
                 
                 if ~isempty(testNum)
-                pars = vertcat(vox.params);
-                sData(s,1) = nanmean(pars(:,testNum));
-                pars2 = vertcat(vox2.params);
-                sData(s,2) = nanmean(pars2(:,testNum));
+                    pars = vertcat(vox.params);
+                    sData(s,1) = nanmean(pars(:,testNum));
+                    pars2 = vertcat(vox2.params);
+                    sData(s,2) = nanmean(pars2(:,testNum));
                 else
-                    eval(['sData(s,1) = nanmean([vox.' test ']);']);
-                    eval(['sData(s,2) = nanmean([vox2.' test ']);']);
+                    eval(['sData(s,1) = nan' whichM '([vox.' test ']);']);
+                    eval(['sData(s,2) = nan' whichM '([vox2.' test ']);']);
                 end
             end
             
@@ -61,6 +66,7 @@ for cc = 1:size(comps,1)
             comp(c).p(r) = P;
             comp(c).stats{r} = STATS;
             if H sig = '***'; else sig = ''; end
-            fprintf('%s [%s %s:] %s param, %s: t(%d)=%.2f, p=%.3f\n',sig,hemText(hems),ROIs{r},test,comp(c).descr,STATS.df,STATS.tstat,P);
+            fprintf('%s [%s %s:] %s param %s, %s: t(%d)=%.2f, p=%.3f\n',sig,hemText(hems),ROIs{r},test,comp(c).descr,whichM,STATS.df,STATS.tstat,P);
         end
     end
+end
