fb3d5d469ed123c9aafd20509bfb3680c6ec7350
Sonia Poltoratski <soniapolt@gmail.com>
2020-03-06 18:44:13 -0800

    debugging deltaDist

--- cssPlot_pRFpars_deltaDist_indivSubjs.m
+++ cssPlot_pRFpars_deltaDist_indivSubjs.m
@@ -2,21 +2,21 @@
 
 clear all; close all;
 
-subjs = prfSubjs;%{'MG' 'JG' 'TH' 'EM' 'DF' 'SP'};%
+subjs = prfSubjs;
 expt = 'fixPRF';
 
-saveFig =1;
+saveFig = 0;
 convertDVA = 1;
 
 minR2 = 'r2-20';          % cutoff for vox selection
-ROIs= ['V1' standardROIs('face')]
+ROI = standardROIs(7);%['V1' standardROIs('face')]
 
 whichStim = 'outline';%'photo';%'internal';%'eyes';%
 whichModel = 'kayCSS';%'inflipCSSn';%'flipCSSn';%'cssExpN';%'cssShift';%
 whichM = 3; % 1 = mean, 2 = mode/peak, 3 = median
 mS = {'mean' 'mode' 'median'};
-plotPars = {'gain' 'r2' 'Y' 'X' 'size'};%
-parTitles = {'Gain Estim' 'Estimated R^{2}' 'Y Estim.' 'X Estim.' 'Size [2xSD/sqrt(N)] (dva)'};
+plotPars = {'Y'};%{'gain' 'r2' 'Y' 'X' 'size'};%{'Ydeg'}%
+parTitles = {'Y estim'};%{'Gain Estim' 'Estimated R^{2}' 'Y Estim.' 'X Estim.' 'Size [2xSD/sqrt(N)] (dva)'};
 
 hems = {'lh' 'rh'};
 fitSuffix = '';%'_orig';%
@@ -30,7 +30,7 @@ nBins = 20; % histogram bins
 
 % now we load in the data from both hemispheres, and threshold across
 load(pRFfile(dirOf(pwd),expt,minR2,whichStim,whichModel,hems,fitSuffix));
-ROInum = cellNum(ROIs,info.ROIs);
+ROInum = cellNum(ROI,info.ROIs);
 subjNum = cellNum(subjs,info.subjs);
 
 if length(subjNum) == 1 roi = subj(subjNum).roi; else subj = subj(subjNum); end
@@ -46,48 +46,50 @@ for p = 1:length(plotPars)
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if saveFig && onLaptop figSize = [.1 .1 .8 .8]; else figSize = [.2 .1 8 .8]; end
-    niceFig(figSize,fontSize);
-    numPlots = [2 ceil(length(ROIs)/2)];pl = 1;
+    f1 = niceFig(figSize,fontSize);
+    numPlots = [2 ceil(length(subjs)/2)];pl = 1;
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for r = 1:length(ROIs)
+    
+    sPars = []; mPars = [];
+    for s = 1:length(subjs)
         
         subplot(numPlots(1),numPlots(2),pl)
         
-        sPars = []; mPars = [];
         for c = 1:length(roi(1).fits)
-            for s = 1:length(subjNum)
-                fits = subj(subjNum(s)).roi(ROInum(r)).fits(c);
-                
-                try
+            
+            fits = subj(subjNum(s)).roi(ROInum).fits(c);
+            
+            try
                 cPars{c} = getPar(plotPars{p},fits,1);
-                catch cPars{c} = NaN; end % missing ROIs
-                
-                % aggregate across subjects
-                sPars{s,c} = cPars{c}; % full distribution for this subject
-                eval(['mPars(s,c) = nan' mS{whichM} '(cPars{c});']); % mean value for this subject
-            end
+            catch cPars{c} = NaN; end % missing ROIs
+            
+            % aggregate across subjects
+            sPars{s,c} = cPars{c}; % full distribution for this subject
+            eval(['mPars(s,c) = nan' mS{whichM} '(cPars{c});']); % mean value for this subject
         end
         hues = [1 .5];
         if containsTxt(plotPars{p},'size')
             xl = [0 5] ;
         else xl = [-2 2]; end
-        for n = 1:length(sPars)
-            dPars{n} = sPars{n,1}-sPars{n,2};
-        end
-        [h,meds, normcounts] = plotMeanDistr(dPars,nBins,roiColors(ROIs(r)),1); %xlim(xl);
+        dPars{s} = sPars{s,1}-sPars{s,2};
+        
+        plotDistr({dPars{s}},1,{''},20);
+        xlabel('Inverted minus Upright');
         set(gca,'TickDir','out');
         pl = pl+1;
         axis square;
         
-        [h,pv,ci] = ttest(mPars(:,2)-mPars(:,1));
-        title({ROIs{r};parTitles{p};['ttest on medians: p=' num2str(pv)]},'fontSize',titleSize,'interpreter','none','FontWeight','bold');
+        title({subjs{s};parTitles{p}});
     end
+    
     superTitle(titleText,titleSize,.025);
     
+    f2=figure;
+    [h,meds, normcounts] = plotMeanDistr(dPars,nBins,roiColors(ROI),1); %xlim(xl);
     if saveFig == 1
-        if length(subjs) == 1 txt = ['subj' subjs{1}]; else txt = ['groupN' num2str(length(subjs))]; end
-        txt = [plotPars{p} '_' hemText(hems) '_' txt];
+        figure(f(1));
+        txt = [plotPars{p} '_' ROI 'indivSubjs_' hemText(hems)];
         niceSave([dirOf(pwd) 'figures/' expt '/deltaDist/'],txt,[],[],{'png' 'svg'}); % just save pngs, since these can be generated pretty quickly
     end
 end
